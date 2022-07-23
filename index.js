f3a15e591bdb4fa09fcd8b5d861666531fae21c4
ImranRH321 <imranrh247@gmail.com>
2022-07-23 19:48:18 +0600

    user_admin

--- index.js
+++ index.js
@@ -94,6 +94,20 @@ async function run() {
       res.send(result);
     });
 
+    // __________make_admin________
+    app.put("/user/admin/:id", async (req, res) => {
+      const email = req.params.id;
+      console.log(email);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // -------------------UpdateProfile____________
     app.put("/myProfile", async (req, res) => {
       const email = req.query.email;
